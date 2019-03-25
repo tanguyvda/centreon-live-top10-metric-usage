c6ec4c7ebec63e3a024722c78f72e0533419e0f5
tanguyvda <tanguyvda@hotmail.fr>
2019-03-25 10:55:17 +0100

    add the option to display the metric value on the chart bars #6

--- src/apexcharts_test.js
+++ src/apexcharts_test.js
@@ -1,15 +1,23 @@
-function load_graph_apex(data) {
+function load_graph_apex(data, preferences) {
 console.log(data);
+console.log(preferences);
 var metric_value = new Array();
 var host_name = new Array();
+
 for (i in data) {
-metric_value.push(data[i].current_value);
-host_name.push(data[i].host_name);
-console.log(metric_value);
+	metric_value.push(data[i].current_value);
+	host_name.push(data[i].host_name);
 }
+
+if (preferences.display_metric_value == 1) {
+	var display_metric_value=true;
+} else {
+	var display_metric_value=false;
+}
+	
      var options = {
             chart: {
-                height: 350,
+                height: preferences.height,
                 type: 'bar',
             },
             plotOptions: {
@@ -18,7 +26,7 @@ console.log(metric_value);
                 }
             },
             dataLabels: {
-                enabled: true,
+                enabled: display_metric_value,
                 position: 'top',
             },
             series: [{
